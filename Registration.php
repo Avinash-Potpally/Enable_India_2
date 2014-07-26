6b5d5b68506b271537da105ffe26e5899cce4b6d
Avinash <anise.avinash@gmail.com>
2014-07-27 00:48:22 +0530

    regback

---
+++ Registration.php
@@ -0,0 +1,32 @@
+    <?
+		 include'connection.php';
+		  $name=$_POST['uname'];
+		  $email=$_POST['email'];
+		  $pw=$_POST['password'];
+		  $qual=$_POST['qual'];
+		  //$status=$-POST['status'];
+		  $dob=$_POST['dob'];
+		  //$addr=$_POST['address'];
+		  $ph=$_POST['tel'];
+		 // $lang=$_POST['language'];
+		  $gen=$_POST['gender'];
+		  $skype=$_POST['skype'];
+		  $fb=$_POST['fb'];
+		  $role=$_POST['role'];
+		 $repwd=$_POST['repassword'];
+		if($repwd==$pw)
+		{
+		 $rowset = mysql_query("insert into user_info values ('','$name','$email','$pw','$qual','','$dob','$ph','','$gen','$skype','$fb','$role','')");
+		 //echo"<script>alert('Success');window.location='./index.php';</script>";
+		 echo"1";
+		}
+		else
+		{
+		//echo"<script>alert('Password Mis-Match');</script>";
+		//header( 'Location: registrationForm.php' ) ;
+		echo "2";
+		
+		}
+		
+		   
+    ?>
